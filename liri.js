c9dab928e9dc07b12a53922ff9393abea6527432
Kristin Herrmann <dickmann.michael@gmail.com>
2018-08-28 15:51:52 -0600

    omdb add

--- liri.js
+++ liri.js
@@ -24,5 +24,28 @@ spotify.search({ type: 'track', query: 'I Want It That Way' }, function(err, dat
       return console.log('Error occurred: ' + err);
     }
    
-  console.log(data); 
+  console.log(data[0]); 
   });
+
+
+
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
+
+var request = require("request");
+
+request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  
+  if (!error && response.statusCode === 200) {
+      var responseBody = JSON.parse(body);
+
+    console.log("The movie's rating is: ", responseBody);
+  }
+});
