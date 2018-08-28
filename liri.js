15be4a54f9f8a2db9de5a9739d5147f51fc28fe4
Kristin Herrmann <dickmann.michael@gmail.com>
2018-08-28 00:43:42 -0600

    read files

--- liri.js
+++ liri.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 
 var spotify = require("/keys.js");
+
+
+
+
+
+var fs = require("fs");
+fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+    return console.log(error);
+}
+  console.log(data);
+  var dataArr = data.split(",");
+  console.log(dataArr);
+});
