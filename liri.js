1de8b9ad6d3d79c76eae78008d9d83d36a2a91c6
Kristin Herrmann <dickmann.michael@gmail.com>
2018-08-29 20:26:27 -0600

    spot-func

--- liri.js
+++ liri.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
-
+ 
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
-
-
-
-
 var fs = require("fs");
+var request = require("request");
 fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
     return console.log(error);
@@ -17,48 +13,53 @@ fs.readFile("random.txt", "utf8", function(error, data) {
   var dataArr = data.split(",");
   console.log(dataArr);
 });
-
-var Spotify = require('node-spotify-api');
  
 
-
-spotify.search({ type: 'track', query: 'I Saw The Sign' }, function(err, data) {
+function getSong(song){
+  console.log("function working");
+  spotify.search({ type: 'track', query: song }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
+    
     }
-   
-  console.log(data[0]); 
+    var songs = data.tracks.items
+    for (var i = 0; i < songs.length; i++){
+      console.log("=============================================");
+      // console.log(songs[i]);
+      console.log(songs[i].artists[0].name);
+    }
+  
   });
-
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
-
-var request = require("request");
-
-var nodeArgs = process.argv;
-var movieName = "";
-for (var i = 2; i < nodeArgs.length; i++) {
-
-  if (i > 2 && i < nodeArgs.length) {
-movieName = movieName + "+" + nodeArgs[i];
-}
-else {
-movieName += nodeArgs[i];
-}
 }
 
+
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
+
+// var nodeArgs = process.argv;
+// var movieName = "";
+// for (var i = 2; i < nodeArgs.length; i++) {
+
+//   if (i > 2 && i < nodeArgs.length) {
+// movieName = movieName + "+" + nodeArgs[i];
+// }
+// else {
+// movieName += nodeArgs[i];
+// }
+// }
+
+function getMovie(movieName){
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
 // request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function(error, response, body) 
 
 request(queryUrl, function(error, response, body){
-
       
     if (!error && response.statusCode === 200) {
       var responseBody = JSON.parse(body);
@@ -66,5 +67,14 @@ request(queryUrl, function(error, response, body){
     console.log("The movie's rating is: ", responseBody);
   }
 });
+};
 
-
+var commands = process.argv[2];
+var parameter = process.argv.slice(3).join(" ");
+console.log(parameter);
+if(commands === "spotify-this-song") {
+  getSong(parameter);
+}
+if(commands === "movie-this") {
+  getMovie(parameter);
+}
